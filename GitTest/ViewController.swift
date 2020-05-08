1062ece7079bfbf5224a8a567abe6ac99475e107
Emre <EMRE@Emre-MacBook-Pro.local>
2020-05-08 23:23:20 +0300

    first commit

---
+++ GitTest/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  GitTest
+//
+//  Created by Emre  on 8.05.2020.
+//  Copyright © 2020 Emre Eravcı. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        print("Github")
+    }
+
+
+
+}
+
